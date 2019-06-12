eecf03deac5669f15842fcc505021bbb788b39c3
bhautikgosai96 <bhautikng143@gmail.com>
2019-06-12 16:29:33 +0530

    Update extend-restapi.php

--- extend-restapi.php
+++ extend-restapi.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * The plugin bootstrap file
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Extend RestApi
+ * Plugin URI:        ABC
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            bhautik gosai
+ * Author URI:        https://github.com/bhautikgosai96/extend-restapi
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       extend-restapi
+ */
 // Extend rest response
 add_action( 'rest_api_init', 'extend_rest_post_response' );
 function extend_rest_post_response() {
